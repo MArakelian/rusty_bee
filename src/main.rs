d06225ee78148c39bfbf4bbcec5d5b487500568d
Mark Arakelian <61424185+MArakelian@users.noreply.github.com>
2025-01-16 18:13:36 -0500

    Make the dictionary a vector of Strings instead of a single string

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::read_to_string;
 use std::path::Path;
+
+fn read_lines(filename: &Path) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
 
 fn main() {
     // create a path to the dictionary file.
     let path = Path::new("assets/words.txt");
-    let display = path.display();
 
-    // Open the path
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
+    let words = read_lines(path);
 
-    // Read the dictionary into a string
-    let mut words = String::new();
-    match file.read_to_string(&mut words) {
-        Err(why) => panic!("Couldn't read {}: {}", display, why),
-        Ok(_) => print!("{}", words),
+    for word in words {
+        println!("{}", word);
     }
 }
 
