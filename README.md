1909aa540860f502e644ccaca2c3936fe3ba33a6
Mark Arakelian <61424185+MArakelian@users.noreply.github.com>
2025-01-16 14:20:12 -0500

    Added file read functionality

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rusty Bee
 
-A program which solves the NYT Spelling Bee game, written in rust. 
+A program which solves the NYT Spelling Bee game, written in rust.
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // create a path to the dictionary file.
+    let path = Path::new("assets/words.txt");
+    let display = path.display();
+
+    // Open the path
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the dictionary into a string
+    let mut words = String::new();
+    match file.read_to_string(&mut words) {
+        Err(why) => panic!("Couldn't read {}: {}", display, why),
+        Ok(_) => print!("{}", words),
+    }
 }
