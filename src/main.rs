842b1058e1f11d295fde3dafac22eb5c49c4fdc5
Mark Arakelian <61424185+MArakelian@users.noreply.github.com>
2025-01-17 10:24:45 -0500

    Added function to get the required letter from the user

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![allow(unused)]
+
 use std::fs::read_to_string;
 use std::path::Path;
+use text_io::read;
 
+/// Reads the words from the dictionary txt file and
+/// returns a Vec<String>
 fn read_lines(filename: &Path) -> Vec<String> {
     read_to_string(filename)
         .unwrap()
@@ -9,19 +14,33 @@ fn read_lines(filename: &Path) -> Vec<String> {
         .collect()
 }
 
+
+/// Gets the center letter required to be in all the
+/// solution words.
+fn get_magic_letter() -> String {
+    print!("Please enter the center letter: ");
+    let magic_letter: String = read!();
+    magic_letter
+}
+
+
+
 fn main() {
     // create a path to the dictionary file.
     let path = Path::new("assets/words.txt");
 
+    // create the dictionary of words (a Vec<String>)
     let words = read_lines(path);
 
-    for word in words {
-        println!("{}", word);
-    }
+    // get the magic letter
+    let required_letter = get_magic_letter();
+    println!("The required letter is: {}", required_letter);
 }
 
 //  TODO:
 //  Add features:
-//  - [ ] read in letter required in all words
+//  - [X] create a Vec<String> of words instead of one long String
+//  - [X] read in letter required in all words
 //  - [ ] read in letter set with which to make solution words.
 //  - [ ] iterate over words to select solutions.
+//  - [ ] handle different file paths for Windows
