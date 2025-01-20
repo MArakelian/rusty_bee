b405ba300100f1aadeb42e2bd5536e71c155ef53
Mark Arakelian <61424185+MArakelian@users.noreply.github.com>
2025-01-20 14:26:04 -0500

    Add filter to include only words with the required letter, add required letter to the required letter set

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,22 @@
 
 use std::fs::read_to_string;
 use std::path::Path;
-use text_io::{read, scan};
+use text_io::read;
 
 static DICTIONARY_WORDS: &str = include_str!("../assets/words.txt");
 
 /// Create the dictionary of words as a Vec<String>
-fn create_string(dict_words: &str) -> Vec<String> {
+fn create_string(dict_words: &str, required_letter: &String) -> Vec<String> {
     let initial_dictionary: Vec<String> = dict_words.lines().map(String::from).collect();
 
     let mut processed_dictionary = Vec::new();
     for word in initial_dictionary {
-        if word.len() > 3 {
+        // filter out words less than 3 letters long and
+        // filter out words without the required letter.
+        if word.len() > 3 && word.contains(&required_letter.as_str()) {
             processed_dictionary.push(word);
         }
     }
-
     processed_dictionary
 }
 
@@ -38,21 +39,23 @@ fn get_required_letters() -> Vec<String> {
     let mut required_letters_processed: Vec<String> =
         required_letter_raw.split("").map(String::from).collect();
 
-    // let required_letters_processed: Vec<String> = required_letters_raw.split(' ').map(String::from).collect();
-    required_letters_processed
+    required_letters_processed.remove(0); //remove empty string at beginning
+    required_letters_processed.pop(); //remove empty string at start
+    required_letters_processed //return required
 }
 
 fn main() {
-    // create a dictionary of words
-    let words = create_string(DICTIONARY_WORDS);
-
     // get the magic letter
     let required_letter = get_magic_letter();
     println!("The required letter is: {}", required_letter);
 
+    // create a dictionary of words which are longer than 3 letters
+    // and only contain the required letter
+    let words = create_string(DICTIONARY_WORDS, &required_letter);
+
     // get the other required letters
-    let other_required_letters = get_required_letters();
-    println!("{:?}", other_required_letters);
+    let mut required_letter_set = get_required_letters();
+    required_letter_set.push(required_letter);
 }
 
 //  TODO:
